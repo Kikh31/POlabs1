180194f05230b300e96d74d44aefd75761bd2518
Kikh31 <artem.kikhtenko33@gmail.com>
2022-12-05 12:04:10 +0300

    commit

--- tests/calcTests.cpp
+++ tests/calcTests.cpp
@@ -9,7 +9,7 @@ TEST_CASE("plus test") {
     SUBCASE("--") CHECK(cplus(-1, -1) == -2);
     SUBCASE("+-") CHECK(cplus(1, -1) == 0);
     SUBCASE("-+") CHECK(cplus(-1, 1) == 0);
-    //SUBCASE("broke") CHECK(brokePlus(1, 1) == 2);
+    SUBCASE("broke") CHECK(brokePlus(1, 1) == 2);
 }
 
 TEST_CASE("minus test") {
